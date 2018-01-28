ed4a15efc81515bfa5842286d9550cfc28aa4bd0
Henry Desroches <yo@henry.codes>
2018-01-28 14:00:42 -0700

    Add cool styles for #16, #17, #19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,6 +3,10 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+const shutter = document.querySelector(".take-photo");
+const buttons = document.querySelectorAll("a");
+const effectControls = document.querySelectorAll(".controls");
+const colorizeControls = document.querySelectorAll('input[name="colorize"]');
 
 const getVideo = () => {
   navigator.mediaDevices
@@ -18,7 +22,28 @@ const getVideo = () => {
       console.error(`Oh FUCK`, err);
     });
 };
-
+let activeEffect = null;
+const handleControls = e => {
+  e.preventDefault();
+  const control = e.target.dataset.control;
+  if (control === "takePhoto") {
+    takePhoto();
+    return;
+  } else if (control === activeEffect) {
+    activeEffect = null;
+    buttons.forEach(button => button.classList.remove("active"));
+    effectControls.forEach(controls => controls.classList.remove("active"));
+  } else {
+    buttons.forEach(button => button.classList.remove("active"));
+    e.target.classList.add("active");
+    activeEffect = control;
+    effectControls.forEach(controls => controls.classList.remove("active"));
+    const activeControls = document.querySelector(
+      `[data-effect=${activeEffect}]`
+    );
+    activeControls ? activeControls.classList.add("active") : null;
+  }
+};
 const paintToCanvas = () => {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -28,9 +53,16 @@ const paintToCanvas = () => {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
-    // redEffect(pixels);
-    spectrascope(pixels);
-    // greenScreen(pixels);
+    if (activeEffect === "colorize") {
+      ctx.globalAlpha = 1;
+      colorize(pixels);
+    } else if (activeEffect === "spectrascope") {
+      ctx.globalAlpha = 0.3;
+      spectrascope(pixels);
+    } else if (activeEffect === "chromakey") {
+      ctx.globalAlpha = 1;
+      chromakey(pixels);
+    }
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 };
@@ -43,17 +75,23 @@ const takePhoto = () => {
   const link = document.createElement("a");
   link.href = data;
   link.setAttribute("download", "sick-as-fuck");
+  link.classList.add("photo");
   link.innerHTML = `<img src="${data}" alt="Sick as fuck duder" />`;
   strip.insertBefore(link, strip.firstChild);
 };
 
-const redEffect = pixels => {
+const colorize = pixels => {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 100;
-    pixels.data[i + 1] = pixels.data[i + 1] - 50;
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    pixels.data[i + 0] = pixels.data[i + 0] + colorizeColor[0] * 100;
+    pixels.data[i + 1] = pixels.data[i + 1] + colorizeColor[1] * 100;
+    pixels.data[i + 2] = pixels.data[i + 2] + colorizeColor[2] * 100;
   }
   return pixels;
+};
+const handleColorize = e => {
+  colorizeColor = e.target.dataset.factor
+    .split(", ")
+    .map(val => parseFloat(val));
 };
 
 const spectrascope = pixels => {
@@ -62,10 +100,10 @@ const spectrascope = pixels => {
     pixels.data[i + 100] = pixels.data[i + 1];
     pixels.data[i - 100] = pixels.data[i + 2];
   }
-  ctx.globalAlpha = 0.2;
+  return pixels;
 };
 
-const greenScreen = pixels => {
+const chromakey = pixels => {
   const levels = {};
 
   document.querySelectorAll(".rgb input").forEach(input => {
@@ -95,4 +133,10 @@ const greenScreen = pixels => {
 
 getVideo();
 
+let colorizeColor = [1, -1, -1];
+colorizeControls.forEach(input =>
+  input.addEventListener("change", handleColorize)
+);
+
+buttons.forEach(button => button.addEventListener("click", handleControls));
 video.addEventListener("canplay", paintToCanvas);
